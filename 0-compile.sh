a5b206f96b9cc2b7e426a174366d44ff3da799a3
Marcus Sackrow <alb42@web.de>
2021-12-19 22:28:25 +0100

    Catalog files for package

--- 0-compile.sh
+++ 0-compile.sh
@@ -4,6 +4,9 @@ flexcat locale/AmiTube.cd Catalogs/deutsch.ct NEWCTFILE Catalogs/deutsch.ct
 flexcat locale/AmiTube.cd Catalogs/deutsch.ct CATALOG Catalogs/deutsch/AmiTube.catalog
 flexcat locale/AmiTube.cd AmiTubelocale.pas=locale/FPCUnit.sd
 
+cp locale/AmiTube.cd pack/AmiTube/Catalogs
+cp Catalogs/deutsch/AmiTube.catalog pack/AmiTube/Catalogs/deutsch
+
 rm -f ./lib/m68k-amiga/*
 fpc4amiga000.sh -B -FU./lib/m68k-amiga -Fu../MUIClass/src AmiTube.pas -o./pack/AmiTube/AmiTube.000
 
