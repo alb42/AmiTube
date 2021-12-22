e62e7c20556d6178a7c028da04579d40e422731d
Marcus Sackrow <alb42@web.de>
2021-12-22 12:46:42 +0100

    german locale move to other place

--- 0-compile.sh
+++ 0-compile.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
-flexcat locale/AmiTube.cd Catalogs/deutsch.ct NEWCTFILE Catalogs/deutsch.ct
-flexcat locale/AmiTube.cd Catalogs/deutsch.ct CATALOG Catalogs/deutsch/AmiTube.catalog
 flexcat locale/AmiTube.cd AmiTubelocale.pas=locale/FPCUnit.sd
+# languages
+flexcat locale/AmiTube.cd locale/deutsch.ct NEWCTFILE locale/deutsch.ct
+flexcat locale/AmiTube.cd locale/deutsch.ct CATALOG Catalogs/deutsch/AmiTube.catalog
 
+# copy locales to package
 cp locale/AmiTube.cd pack/AmiTube/Catalogs
-cp Catalogs/deutsch/AmiTube.catalog pack/AmiTube/Catalogs/deutsch
+cp -r Catalogs/* pack/AmiTube/Catalogs/
 
 rm -f ./lib/m68k-amiga/*
 fpc4amiga000.sh -B -FU./lib/m68k-amiga -Fu../MUIClass/src AmiTube.pas -o./pack/AmiTube/AmiTube.000
