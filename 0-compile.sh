20ad1a59ef2de4be7fa0e3854686b3bb0f7b5f4e
Marcus Sackrow <alb42@web.de>
2021-12-26 22:59:51 +0100

    AROS compilation added

--- 0-compile.sh
+++ 0-compile.sh
@@ -9,9 +9,23 @@ flexcat locale/AmiTube.cd locale/deutsch.ct CATALOG Catalogs/deutsch/AmiTube.cat
 cp locale/AmiTube.cd pack/AmiTube/Catalogs
 cp -r Catalogs/* pack/AmiTube/Catalogs/
 
+# copy locales to package
+cp locale/AmiTube.cd pack/AmiTubeAROS/Catalogs
+cp -r Catalogs/* pack/AmiTubeAROS/Catalogs/
+
 rm -f ./lib/m68k-amiga/*
 fpc4amiga000.sh -B -FU./lib/m68k-amiga -Fu../MUIClass/src AmiTube.pas -o./pack/AmiTube/AmiTube.000
 
 rm -f ./lib/m68k-amiga/*
 fpc4amiga.sh -B -FU./lib/m68k-amiga -Fu../MUIClass/src AmiTube.pas -o./pack/AmiTube/AmiTube
+
+mkdir -p lib/i386-aros
+rm -f ./lib/i386-aros/*
+fpc4aros.sh -B -FU./lib/i386-aros -Fu../MUIClass/src AmiTube.pas -o./pack/AmiTubeAROS/AmiTube.i386
+
+mkdir -p lib/arm-aros
+rm -f ./lib/arm-aros/*
+fpc4arosarm.sh -B -FU./lib/arm-aros -Fu../MUIClass/src AmiTube.pas -o./pack/AmiTubeAROS/AmiTube.arm
+
+
 
--- pack/packme.sh
+++ pack/packme.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm -f AmiTube.lha
+rm -f AmiTubeAROS.lha
 rm -f AmiTube/*.uaem
 
 lha ao5 AmiTube.lha AmiTube AmiTube.info
+lha ao5 AmiTubeAROS.lha AmiTubeAROS AmiTubeAROS.info
